120fb0e3544e85bc390341f1224380cba1c67dba
ctg <kamilayanaul@mail.ru>
2023-01-13 14:54:52 +0300

    Add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return(
+        <footer>
+            <h1>Подвал</h1>
+            <p>проект Фатиховой Камила 2023</p>
+        </footer>
+    );
+}
+
+export default Footer;
