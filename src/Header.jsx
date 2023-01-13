d242bccca4b5ccdf273cddb294ca03966afaf97b
ctg <kamilayanaul@mail.ru>
2023-01-13 14:30:01 +0300

    Init react app

---
+++ src/Header.jsx
@@ -0,0 +1,28 @@
+// function Header() {
+//     return(
+//         <header>
+//             <h1>Header</h1>
+//             <p>This is paragraph</p>
+//         </header>
+//     );
+// }
+// export default Header;
+
+// export function Header() {
+//     return(
+//         <header>
+//             <h1>Header</h1>
+//             <p>This is paragraph</p>
+//         </header>
+//     );
+// }
+
+const Header = () => {
+    return(
+        <header>
+            <h1>Header</h1>
+            <p>This is paragraph</p>
+        </header>
+    );
+}
+export default Header;
